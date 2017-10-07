579d098814f2f2fb6b58040b3f7c51dc56ee0d5c
Nicolò Fuccella <nico.fuccella@gmail.com>
2017-10-07 15:59:59 +0200

    piechart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import Navbar from './components/Navbar'
 import DataGrid from './components/DataGrid'
 import getResults from './helpers/getResults'
+import FieldPieChart from './components/FieldPieChart'
 
 class App extends Component {
   constructor(props) {
@@ -22,6 +23,7 @@ class App extends Component {
         <div>
             <Navbar/>
             <DataGrid data = { this.state.data }/>
+            <FieldPieChart data = { this.state.data } field = { 'Valid HTTPS' } expected = { 'True' } />
         </div>
     );
   }
