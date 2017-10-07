fdeda6af6508e6fec5f3ea0c3612b76602f38219
denis.goryaynov <denis.gor27@gmail.com>
2017-10-07 14:03:06 +0200

    Css

---
+++ src/components/Navbar.js
@@ -0,0 +1,84 @@
+import React from 'react'
+
+const Navbar = () => (
+    <nav className="Megamenu Megamenu--default js-megamenu ">
+        <ul className="Megamenu-list">
+            <li className="Megamenu-item">
+                <a href="#">
+                    Maiores eius et<br/>Aut dolores exercitationem
+                </a>
+                <div className="Megamenu-subnav u-jsDisplayNone">
+                    <ul className="Megamenu-subnavGroup">
+                        <li><a href="/">Aut dolores exercitationem</a>
+                            <ul>
+                                <li><a href="/1">Rerum at labore</a></li>
+                                <li><a href="/2">Modi nostrum expedita</a></li>
+                                <li><a href="/3">Et dolores quae</a></li>
+                                <li><a href="/4">Quaerat enim saepe</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="">Error ut harum</a></li>
+                    </ul>
+                    <ul className="Megamenu-subnavGroup">
+                        <li><a href="">Magni voluptatem non</a>
+                            <ul>
+                                <li><a href="/1">Omnis quaerat vitae</a></li>
+                                <li><a href="/2">Deleniti voluptatem pariatur</a></li>
+                                <li><a href="/3">Maiores eius et</a></li>
+                                <li><a href="/4">Aut dolores exercitationem</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="">Rerum at labore</a></li>
+                    </ul>
+                    <ul className="Megamenu-subnavGroup">
+                        <li><a href="">Modi nostrum expedita</a>
+                            <ul>
+                                <li><a href="/1">Et dolores quae</a></li>
+                                <li><a href="/2">Quaerat enim saepe</a></li>
+                                <li><a href="/3">Error ut harum</a></li>
+                                <li><a href="/4">Magni voluptatem non</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="">Omnis quaerat vitae</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li className="Megamenu-item">
+                <a href="#">Deleniti voluptatem pariatur</a>
+                <div className="Megamenu-subnav u-jsDisplayNone">
+                    <ul className="Megamenu-subnavGroup">
+                        <li><a href="/1">Maiores eius et</a></li>
+                        <li><a href="/2">Aut dolores exercitationem</a></li>
+                    </ul>
+                    <ul className="Megamenu-subnavGroup">
+                        <li><a href="/1">Rerum at labore</a></li>
+                        <li><a href="/2">Modi nostrum expedita</a></li>
+                    </ul>
+                    <ul className="Megamenu-subnavGroup">
+                        <li><a href="/1">Et dolores quae</a></li>
+                        <li><a href="/2">Quaerat enim saepe</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li className="Megamenu-item">
+                <a href="#">Error ut harum</a>
+                <div className="Megamenu-subnav u-jsDisplayNone">
+                    <ul className="Megamenu-subnavGroup">
+                        <li><a href="/1">Magni voluptatem non</a></li>
+                        <li><a href="/2">Omnis quaerat vitae</a></li>
+                    </ul>
+                    <ul className="Megamenu-subnavGroup">
+                        <li><a href="/1">Deleniti voluptatem pariatur</a></li>
+                        <li><a href="/2">Maiores eius et</a></li>
+                    </ul>
+                    <ul className="Megamenu-subnavGroup">
+                        <li><a href="/1">Aut dolores exercitationem</a></li>
+                        <li><a href="/2">Rerum at labore</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </nav>
+)
+
+export default Navbar
