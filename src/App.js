1b681c763dae54309a3b04ee90d76ef4c301fe69
denis.goryaynov <denis.gor27@gmail.com>
2017-10-07 14:38:57 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react'
-import { csv } from 'd3-request'
 import Navbar from './components/Navbar'
 
 class App extends Component {
-  componentDidMount() {
-    csv('./results/pshtt.csv', (error))
-  }
   render() {
     return (
         <div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,37 @@
 import React from 'react'
 
 const Navbar = () => (
-    <nav className="Megamenu Megamenu--default js-megamenu ">
-        <div className="Megamenu-item">
-            <a href="#">
-                Maiores eius et<br/>Aut dolores exercitationem
-            </a>
+  <header className="Header  u-hiddenPrint">
+
+    <div className="Header-navbar ">
+      <div className="u-layout-wide Grid Grid--alignMiddle u-layoutCenter">
+
+        <div className="Header-title Grid-cell">
+          <h1 className="Header-titleLink">
+            <a href="/">Dashboard</a>
+          </h1>
         </div>
-    </nav>
+
+        <div className="Header-searchTrigger Grid-cell">
+          <button aria-controls="header-search" className="js-Header-search-trigger Icon Icon-search "
+                  title="attiva il form di ricerca" aria-label="attiva il form di ricerca" aria-hidden="false">
+          </button>
+          <button aria-controls="header-search" className="js-Header-search-trigger Icon Icon-close u-hidden "
+                  title="disattiva il form di ricerca" aria-label="disattiva il form di ricerca" aria-hidden="true">
+          </button>
+        </div>
+
+        <div className="Header-toggle Grid-cell">
+          <a className="Hamburger-toggleContainer js-fr-offcanvas-open u-nojsDisplayInlineBlock u-lg-hidden u-md-hidden"
+             href="#menu" aria-controls="menu" aria-label="accedi al menu" title="accedi al menu">
+            <span className="Hamburger-toggle" role="presentation"/>
+            <span className="Header-toggleText" role="presentation">Menu</span>
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </header>
 )
 
 export default Navbar
